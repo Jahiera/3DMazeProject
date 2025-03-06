3016152053e05ffab53f6991ccc0fd67b748cca9
Jahiera <jasminejahieravassallo@gmail.com>
2025-03-06 16:01:14 +0100

    Player health and health text

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
    
     public int points = 5;
+    public Vector3 respawnPosition;
+    public TMP_Text healthText;
 
+    private void Start()
+    {
+        //move player to the start
+        respawnPosition = transform.position;
+        //reset points to 5
+        points = 5;
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Trap"))
@@ -12,19 +23,24 @@ public class Health : MonoBehaviour
             Damage(1);
             
         }
+
+        if (other.CompareTag("Checkpoint"))
+        {
+            //we can set the position based on the checkpoint so that this acts as a guaranteed safe spot.
+            respawnPosition = other.transform.position;
+            respawnPosition.y = transform.position.y;
+        }
+        
     }
     //to remove some health points
     private void Damage(int value)
     {
         points = points - value;
+        healthText.text = $"<b>Health</b>: {points}";
         if(points < 1)
-
-        //HW: 
-        //Do Not Destroy
-        //Move the player to the start
-        //Reset points to 5
         {
-            Destroy(gameObject);
+           transform.position = respawnPosition;
+           points = 5;
         }
     }
 
--- Assets/Scripts/PlayerCollection.cs
+++ Assets/Scripts/PlayerCollection.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerCollection : MonoBehaviour
 {
+    
+    //add scoring system here
     private void OnTriggerEnter(Collider other)
     {
         //if the object is TAGGED with "COLLECTABLE", then it is destroyed
